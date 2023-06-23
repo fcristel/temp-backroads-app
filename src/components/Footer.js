b2d5941ec31a322d2f165725ff698a5a5cabbd98
fcristel <cristel_f@yahoo.com>
2023-06-23 20:06:10 +0200

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      {/* <ul className="footer-links">
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className="footer-link">
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a
+                href={link.href}
+                target="_blank"
+                rel="noreferrer"
+                className="footer-icon"
+              >
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
